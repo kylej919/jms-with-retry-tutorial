cd704df4fd0bae4c6cedb8f78705f089f76d1e24
Kyle Johnson <119340322+kylejPomelo@users.noreply.github.com>
2024-07-07 11:10:31 -0600

    initial commit

---
+++ src/main/java/com/kylej/jms_retry_tutorial/JmsRetryTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.kylej.jms_retry_tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmsRetryTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmsRetryTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kylej/jms_retry_tutorial/JmsRetryTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kylej.jms_retry_tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmsRetryTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
